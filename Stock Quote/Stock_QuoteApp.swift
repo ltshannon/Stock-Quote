8d69ec7ef058f4cd669eaad389c79842a39e5183
Larry Shannon <larry@breakawaydesign.com>
2021-02-25 11:27:09 -0800

    Initial Commit

---
+++ Stock Quote/Stock_QuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_QuoteApp.swift
+//  Stock Quote
+//
+//  Created by Larry Shannon on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Stock_QuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
